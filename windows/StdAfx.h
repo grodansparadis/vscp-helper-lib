0229e48bbc7312a05c6d04bf9384d120f382c33f
Ake Hedman <akhe@grodansparadis.com>
2021-10-08 09:28:13 -0500

    Moved the prebult header files to windows folder where they belong ved

--- windows/StdAfx.h
+++ windows/StdAfx.h
@@ -15,6 +15,9 @@
 #define WIN32_LEAN_AND_MEAN
 #endif
 
+// TODO: Should be handled in placed
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+
 #include <windows.h>
 #include <winsock2.h>
 #include <ws2tcpip.h>
