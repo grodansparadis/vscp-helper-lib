fbc11db52da3be18f91bdc0c49920b68bae9f99a
Ake <akhe@grodansparadis.com>
2021-10-18 10:08:31 +0200

    test: Fixed connect test

--- tests/connecttest/unittest.cpp
+++ tests/connecttest/unittest.cpp
@@ -11,21 +11,16 @@
 #include <vscphelperlib.h>
 #include <gtest/gtest.h>
 
-// Output text for happy moments if set to one
-#define verbose_success		0
-
-// Output text for sad moments if set to one
-#define verbose_error		1
-
-// Output text for talky moments if set to 1
-#define verbose_info		1
+#define RUN_COUNT 100
 
 // Standard connection (a VSCP daemon must be running here)
 //#define INTERFACE1_HOST         "127.0.0.1:9598"
 //#define INTERFACE1_HOST         "192.168.1.44:9598"  // pi11
-#define INTERFACE1_HOST         "192.168.1.34:9598"  // pi6
-#define INTERFACE1_USER 	"admin"
-#define INTERFACE1_PASSWORD "secret"
+//#define INTERFACE1_HOST       "192.168.1.34:9598"  // pi6
+
+#define INTERFACE1_HOST       "vscp2.vscp.org:9598"
+#define INTERFACE1_USER 	    "admin"
+#define INTERFACE1_PASSWORD   "secret"
 
 TEST(tcpif, Connect_to_VSCP_daemon) { 
 
@@ -61,7 +56,6 @@ TEST(tcpif, Connect_to_VSCP_daemon) {
 
     return;
 
-    //vscpEvent *pEvent;
 
     // Send event on channel 1
     vscpEvent e;
@@ -84,19 +78,19 @@ TEST(tcpif, Connect_to_VSCP_daemon) {
     memset(e.GUID, 0, sizeof(e.GUID) ); // Setting GUID to all zero tell interface to use it's own GUID
 
     if ( VSCP_ERROR_SUCCESS == (rv = vscphlp_sendEvent( handle1, &e ) ) ) {
-        if (verbose_success) printf( "Command success: vscphlp_sendEvent on channel 1\n" );
+        printf( "Command success: vscphlp_sendEvent on channel 1\n" );
     }
     else {
-        if (verbose_error) printf("\aCommand error: vscphlp_sendEvent on channel 1  Error code=%d\n", rv);
+        printf("\aCommand error: vscphlp_sendEvent on channel 1  Error code=%d\n", rv);
         error_cnt++;
     }
 
     // Do it again
     if ( VSCP_ERROR_SUCCESS == (rv = vscphlp_sendEvent( handle1, &e ) ) ) {
-        if (verbose_success) printf( "Command success: vscphlp_sendEvent on channel 1\n" );
+        printf( "Command success: vscphlp_sendEvent on channel 1\n" );
     }
     else {
-        if (verbose_error) printf("\aCommand error: vscphlp_sendEvent on channel 1  Error code=%d\n", rv);
+        printf("\aCommand error: vscphlp_sendEvent on channel 1  Error code=%d\n", rv);
         error_cnt++;
     }
 
@@ -119,24 +113,25 @@ TEST(tcpif, Connect_to_VSCP_daemon) {
     // if ( NULL != pEvent) free( e.pdata );
 
     if ( VSCP_ERROR_SUCCESS == vscphlp_close( handle1 ) ) {
-        if (verbose_success) printf( "Command success: vscphlp_close on channel 1\n" );
+        printf( "Command success: vscphlp_close on channel 1\n" );
     }    
 
     vscphlp_closeSession( handle1 );
 
-    ASSERT_EQ(0, 0);
-
 }
+
+//-----------------------------------------------------------------------------
 
 int main(int argc, char* argv[])
 {
-    int count = 10000;
+    int count = RUN_COUNT;
 
     if ( argc > 1 ) {
         count = atoi(argv[1]);
     }
     testing::InitGoogleTest(&argc, argv);
     for ( int i=0; i<count; i++ ) {
+        printf("\nRunning test %d/%d\n", i, count);
         if ( 1 == RUN_ALL_TESTS() ) {
             printf( "\nFailed at count = %d(%d)\n\n", i, count);
             return 1;
